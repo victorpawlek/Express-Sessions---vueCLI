882fcf69f3ae3536cc1b60dfb13a295b77722d02
Victor Pawlek <pawlek.v03@htlwienwest.at>
2021-04-22 11:53:01 +0200

    login auf server gemacht

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const users = require('../model/users');
 // enter your code here
 
 router.post('/login', (req, res) => {
   // enter your code here
+  if (email && password) {
+    const user = users.find(
+      (el) => el.email === email && el.password === password
+    );
+    if (user) {
+      req.session.userId = user.id;
+      res.status(200).json({ id: user.id, name: user.name });
+    } else res.status(401).send('Wrong email or password');
+  } else res.status(400).send('Login failed');
 });
 
 router.get('/logout', redirectLogin, (req, res) => {
