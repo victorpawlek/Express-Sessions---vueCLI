1d2dec9f896aca4c38c8fccc1747c914f4e66c3b
Victor Pawlek <pawlek.v03@htlwienwest.at>
2021-04-22 11:42:36 +0200

    express-session in server eingebaut

--- server/app.js
+++ server/app.js
@@ -3,8 +3,17 @@ const morgan = require('morgan');
 const path = require('path');
 const routes = require('./routes');
 const errorHandler = require('./middleware/errorHandler');
+const session = require('express-session');
 
 require('dotenv').config({ path: __dirname + '/.env' });
+
+const {
+  PORT,
+  NODE_ENV,
+  SESSION_LIFETIME,
+  SESSION_NAME,
+  SESSION_SECRET,
+} = process.env;
 
 const app = express();
 
@@ -17,6 +26,20 @@ app.use(express.json());
 let { PORT, NODE_ENV, SESSION_LIFETIME, SESSION_NAME, SESSION_SECRET } = process.env;
 
 // Register middleware for express sessions here
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    name: SESSION_NAME,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: SESSION_LIFETIME * 1000 * 60 * 60,
+      httpOnly: false,
+      sameSite: true,
+      secure: NODE_ENV === 'production',
+    },
+  })
+);
 
 app.use('/', routes);
 
